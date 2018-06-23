0a5d90949ff6d13d4d4d82dbb347fd564f62b841
Asgeir Bjørgan <asgeirbj@samfundet.no>
2018-06-23 11:00:43 +0200

    Add installation script for combine_samples_and_angles.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='combine_samples_and_angles',
+      version='1.0',
+      author='Asgeir Bjorgan',
+      py_modules=['combine_samples_and_angles'],
+     )
