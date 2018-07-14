8f6d89a2da2702dbbdfbe59cb0b2f340978a38ae
Asgeir Bjørgan <asgeirbj@samfundet.no>
2018-07-14 10:52:18 +0200

    Move python modules to subfolder, create encapsulating module.

---
+++ antenna_pattern/__init__.py
@@ -0,0 +1,12 @@
+"""
+Functions and modules for antenna pattern analysis in Python.
+
+combine_samples_and_angles: Basic GNU Radio file and rotor angle measurement
+reading and interpolation.
+
+frequency_analysis: Extraction of frequency of interest from IQ sample
+measurements.
+
+cw_extraction: Extraction of signal level from periodic, deterministic morse
+signals.
+"""
--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='antenna_pattern',
       version='1.0',
       author='Asgeir Bjorgan',
-      py_modules=['combine_samples_and_angles', 'cw_extraction', 'frequency_analysis'],
+      packages=['antenna_pattern']
      )
